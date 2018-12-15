759aa6d73f9dd27959d1793ea201c2b1074ab9f7
Your Name <you@example.com>
2018-12-15 03:42:44 -0600

    report

--- reports/gengraph.sh
+++ reports/gengraph.sh
@@ -19,3 +19,23 @@ set ylabel "colors";
 plot  'heuristic_data_10-50.dat' using 1:3 title 'heuristic colors used' with linespoints, \
       'optimal_data_10-50.dat' using 1:3 title 'optimal colors' with linespoints;
 EOF
+
+gnuplot <<EOF
+set terminal "pdfcairo";
+set title "20 nodes 20% probability of edges";
+set output "t2_time.pdf";
+set xlabel "trial";
+set ylabel "seconds";
+plot  'heuristic_data_20-20.dat' using 1:2 title 'heuristic execution time' with linespoints, \
+      'optimal_data_20-20.dat' using 1:2 title 'optimal execution time' with linespoints;
+EOF
+
+gnuplot <<EOF
+set terminal "pdfcairo";
+set title "20 nodes 20% probability of edges";
+set output "t2_color.pdf";
+set xlabel "trial";
+set ylabel "colors";
+plot  'heuristic_data_20-20.dat' using 1:3 title 'heuristic colors used' with linespoints, \
+      'optimal_data_20-20.dat' using 1:3 title 'optimal colors' with linespoints;
+EOF
